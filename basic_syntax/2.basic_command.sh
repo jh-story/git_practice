a7f25f24b932b0ce8fe2ea89dc28d3444c0dfbdb
jh-story <juhee_6004@naver.com>
2025-06-04 10:31:11 +0900

    수정

--- basic_syntax/2.basic_command.sh
+++ basic_syntax/2.basic_command.sh
@@ -4,8 +4,21 @@ git status
 # .은 모든 수정/추가 사항을 add
 git add .
 # 특정 파일만 add할 경우 (경로까지 포함)
+git add testfolder/test1.txt
 
 # commit을 통해 메시지 타이틀과 메시지 내용을 커밋이력으로 생성
 git commit -m "메시지제목" -m "메시지내용"
 # git commit만 입력하고 엔터시, vi 모드가 됨 -> 첫 줄: 타이틀, 두번째 줄: 내용
 git commit
+
+
+# commit 이력 확인
+git log
+git log --oneline # 간결하게 확인
+
+# 원격저장소로 업로드
+git push origin 브랜치명
+# 충돌 발생시 충돌을 무시하고, 로컬을 기준으로 원격에 덮어쓰기
+git push origin 브랜치명 --force
+
+# 
